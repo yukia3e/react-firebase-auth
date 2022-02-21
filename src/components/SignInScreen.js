35a916f59eccedb2d0b355a5f478ed68daa2bcc5
yukia3e <12268087+yukia3@users.noreply.github.com>
2022-02-21 10:07:01 +0900

    initial commit

---
+++ src/components/SignInScreen.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import firebase from '../firebase';
+
+// Configure FirebaseUI.
+const uiConfig = {
+  signInFlow: 'popup',
+  signInSuccessUrl: '/',
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+  ],
+};
+
+function SignInScreen() {
+  return (
+    <div>
+      <h1>My App</h1>
+      <p>Please sign-in:</p>
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+    </div>
+  );
+}
+
+export default SignInScreen
